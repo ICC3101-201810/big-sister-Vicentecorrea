b90d590f6fd989b97c7211b66b9aba7baa1bd045
= <=>
2018-05-08 16:43:40 -0300

    Segundo comentario

--- LabPOO/Program.cs
+++ LabPOO/Program.cs
@@ -11,14 +11,22 @@ namespace LabPOO
     {
         public static List<Product> cart;
         public static List<Product> market;
+        public static List<Product> receta;
 
         public delegate bool DelegateSupervisarCompra(Product productoASupervisar);
         public event EventHandler<EventArgs> OnSupervisarCompra;
+        /*
+        public bool VerSiEstaEnReceta(Product p)
+        {
+
+        }*/
 
         static void Main(string[] args)
         {
             cart = new List<Product>();
             market = new List<Product>();
+            receta = new List<Product>();
+
             SupplyStore();
             while (true)
             {
@@ -31,6 +39,7 @@ namespace LabPOO
                 Console.WriteLine("\t5. Salir");
                 while (true)
                 {
+                    
                     String answer = Console.ReadLine();
                     if (answer == "1")
                     {
@@ -96,6 +105,12 @@ namespace LabPOO
                     {
                         continue;
                     }
+                    if (receta.Contains(market[answer]))
+                    {
+                        Console.WriteLine("SI ESTA EN LA RECETAAAAAAAAAAAAAAA!");
+                        Thread.Sleep(3000);
+                        
+                    }
                     AddToCart(market[answer]);
                     break;
                 }
@@ -140,29 +155,66 @@ namespace LabPOO
 
         public static void SupplyStore()
         {
-            market.Add(new Product("Leche Entera", 820, 89, "1L"));
-            market.Add(new Product("Gomitas Flipy", 720, 12, "100g"));
-            market.Add(new Product("Mantequilla", 850, 12, "125g"));
-            market.Add(new Product("Crema para hemorroides", 4990, 7, "300cc"));
-            market.Add(new Product("Pimienta", 430, 84, "15g"));
-            market.Add(new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc"));
-            market.Add(new Product("Sal Lobos", 330, 150, "1kg"));
-            market.Add(new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un"));
-            market.Add(new Product("Láminas de Lasaña", 1250, 85, "400g"));
-            market.Add(new Product("Tomate", 1290, 200, "1kg"));
-            market.Add(new Product("Harina", 890, 43, "1kg"));
-            market.Add(new Product("Audifonos Samsung", 5990, 40, "1un"));
-            market.Add(new Product("Pisco Alto del Carmen", 5990, 120, "1L"));
-            market.Add(new Product("Carne Molida", 4390, 15, "500g"));
-            market.Add(new Product("Aceite de Oliva", 1790, 77, "250g"));
-            market.Add(new Product("Sal parrillera", 840, 50, "750g"));
-            market.Add(new Product("Cable HDMI 1m", 3990, 25, "1un"));
-            market.Add(new Product("Queso Rallado Parmesano", 499, 102, "40g"));
-            market.Add(new Product("Vino Blanco Caja", 2790, 84, "2L"));
-            market.Add(new Product("Malla de Cebollas", 1090, 91, "1kg"));
-            market.Add(new Product("Tomates Pelados en lata", 700, 48, "540g"));
-            market.Add(new Product("Queso Parmesano", 3790, 41, "200g"));
-            market.Add(new Product("Bolsa de Zanahorias", 890, 74, "1un"));
+            
+            Product p0 = new Product("Leche Entera", 820, 89, "1L");
+            Product p1 = new Product("Gomitas Flipy", 720, 12, "100g");
+            Product p2 = new Product("Mantequilla", 850, 12, "125g");
+            Product p3 = new Product("Crema para hemorroides", 4990, 7, "300cc");
+            Product p4 = new Product("Pimienta", 430, 84, "15g");
+            Product p5 = new Product("Vino Sauvignon Blanc Reserva Botella", 4150, 23, "750cc");
+            Product p6 = new Product("Sal Lobos", 330, 150, "1kg");
+            Product p7 = new Product("Cuaderno Mi Pequeño Pony", 1290, 50, "1un");
+            Product p8 = new Product("Láminas de Lasaña", 1250, 85, "400g");
+            Product p9 = new Product("Tomate", 1290, 200, "1kg");
+            Product p10 = new Product("Harina", 890, 43, "1kg");
+            Product p11 = new Product("Audifonos Samsung", 5990, 40, "1un");
+            Product p12 = new Product("Pisco Alto del Carmen", 5990, 120, "1L");
+            Product p13 = new Product("Carne Molida", 4390, 15, "500g");
+            Product p14 = new Product("Aceite de Oliva", 1790, 77, "250g");
+            Product p15 = new Product("Sal parrillera", 840, 50, "750g");
+            Product p16 = new Product("Cable HDMI 1m", 3990, 25, "1un");
+            Product p17 = new Product("Queso Rallado Parmesano", 499, 102, "40g");
+            Product p18 = new Product("Vino Blanco Caja", 2790, 84, "2L");
+            Product p19 = new Product("Malla de Cebollas", 1090, 91, "1kg");
+            Product p20 = new Product("Tomates Pelados en lata", 700, 48, "540g");
+            Product p21 = new Product("Queso Parmesano", 3790, 41, "200g");
+            Product p22 = new Product("Bolsa de Zanahorias", 890, 74, "1un");
+            market.Add(p0);
+            market.Add(p1);
+            market.Add(p2);
+            market.Add(p3);
+            market.Add(p4);
+            market.Add(p5);
+            market.Add(p6);
+            market.Add(p7);
+            market.Add(p8);
+            market.Add(p9);
+            market.Add(p10);
+            market.Add(p11);
+            market.Add(p12);
+            market.Add(p13);
+            market.Add(p14);
+            market.Add(p15);
+            market.Add(p16);
+            market.Add(p17);
+            market.Add(p18);
+            market.Add(p19);
+            market.Add(p20);
+            market.Add(p21);
+            market.Add(p22);
+            receta.Add(p8);
+            receta.Add(p17);
+            receta.Add(p2);
+            receta.Add(p13);
+            receta.Add(p18);
+            receta.Add(p20);
+            receta.Add(p22);
+            receta.Add(p19);
+            receta.Add(p14);
+            receta.Add(p15);
+            receta.Add(p4);
+            receta.Add(p10);
+            receta.Add(p0);
         }
 
         public static void ShowRecipe()
@@ -189,6 +241,14 @@ namespace LabPOO
             Console.WriteLine("\tSal");
             Console.WriteLine("\tPimienta\n\n");
             Console.WriteLine("Presiona ENTER para volver al supermercado...");
+
+
+            Console.WriteLine("LISTA DE RECETAAAA\n");
+            foreach (Product p in receta)
+            {
+                Console.WriteLine("el producto se llama {0}", p.Name);
+            }
+            
             ConsoleKeyInfo response = Console.ReadKey(true);
             while (response.Key != ConsoleKey.Enter)
             {
